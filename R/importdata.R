a2bc0c32f32dafbe31e63a2cd296a88988637444
Robert M Flight <r.flight79@gmail.com>
2016-05-31 13:08:14 -0400

    moving peak finding to its own file

--- R/importdata.R
+++ R/importdata.R
@@ -38,17 +38,3 @@ import_xcalibur <- function(xcal_file, sheet = 1){
   xcal_data <- filter_(xcal_data, "!is.na(mz)", "!is.na(intensity)")
   xcal_data
 }
-
-#' return peaks
-#'
-#' returns peaks found by \code{pracma::findpeaks}
-#'
-#' @param avg_spectra the avg spectra data
-#'
-#' @export
-#' @return tbl_df
-pracma_findpeaks <- function(avg_spectra){
-  out_peaks <- pracma::findpeaks(avg_spectra$intensity)
-  mz_peaks <- avg_spectra[out_peaks[, 2], ]
-  mz_peaks
-}
