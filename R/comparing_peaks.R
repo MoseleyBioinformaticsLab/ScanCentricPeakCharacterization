ae735cae46049caa61970734edad7e419bb371b0
Robert M Flight <r.flight79@gmail.com>
2016-06-01 14:34:13 -0400

    finding things that have multiple matches between subject and query

--- R/comparing_peaks.R
+++ R/comparing_peaks.R
@@ -27,3 +27,71 @@ find_closest_peaks <- function(query_data, subject_data, q_name, s_name){
   closest_peaks <- do.call(rbind, closest_peaks)
   tbl_df(closest_peaks)
 }
+
+#' refine closest
+#'
+#' after finding closest peaks, goes through and looks for cases where there are
+#' multiple matches, and selects the smallest difference to keep.
+#'
+#' @param closest_list input data.frame
+#'
+#' @export
+#' @import dplyr
+#' @return list
+refine_closest_list <- function(closest_list){
+  # filters to those entries where there are multiple
+  # query peaks for each subject peak
+  multi_sub_2_query <- filter_(
+    summarise_(
+    group_by_(closest_list, "q_peak"),
+    n_s = "length(unique(s_peak))"
+  ), "n_s > 1")
+
+  multi_sub_2_query <- multi_sub_2_query$q_peak
+  m_s_2_q <- filter(closest_list, q_peak %in% multi_sub_2_query)
+
+  if (nrow(m_s_2_q) > 0) {
+    split_q <- split(m_s_2_q, m_s_2_q$q_peak)
+
+    not_min_s <- lapply(split_q, function(in_split_q){
+      no_zero_split <- filter(in_split_q, mz_diff != 0)
+      min_peak <- no_zero_split$s_peak[which.min(no_zero_split$mz_diff)]
+      unlist(select(filter(no_zero_split, s_peak != min_peak), s_peak), use.names = FALSE)
+    })
+    not_min_s <- unique(unlist(not_min_s, use.names = FALSE))
+
+  } else {
+    not_min_s <- numeric(0)
+  }
+
+
+  # filters to those entries where are multiple subject peaks
+  # for each query peak
+  multi_query_2_sub <- filter_(
+    summarise_(
+    group_by_(closest_list, "s_peak"),
+    n_q = "length(unique(q_peak))"
+  ), "n_q > 1")
+  multi_query_2_sub <- multi_query_2_sub$s_peak
+  m_q_2_s <- filter(closest_list, s_peak %in% multi_query_2_sub)
+
+  if (nrow(m_q_2_s) > 0) {
+    split_s <- split(m_q_2_s, m_q_2_s$s_peak)
+
+    not_min_q <- vapply(split_s, function(in_split_s){
+      no_zero_split <- filter(in_split_s, mz_diff != 0)
+      min_peak <- no_zero_split$q_peak[which.min(no_zero_split$mz_diff)]
+      unlist(select(filter(no_zero_split, q_peak != min_peak), q_peak), use.names = FALSE)
+    })
+
+    not_min_q <- unique(unlist(not_min_q, use.names = FALSE))
+
+  } else {
+    not_min_q <- numeric(0)
+  }
+
+  list(not_min_s = not_min_s,
+       not_min_q = not_min_q)
+}
+
+
