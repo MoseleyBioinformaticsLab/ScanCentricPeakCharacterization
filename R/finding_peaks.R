cdd57f33907a85151832ab9cfc3ec060697e12e1
Robert M Flight <r.flight79@gmail.com>
2016-06-02 23:28:15 -0400

    new function for finding peaks that uses double derivative

--- R/finding_peaks.R
+++ R/finding_peaks.R
@@ -3,11 +3,34 @@
 #' returns peaks found by \code{pracma::findpeaks}
 #'
 #' @param avg_spectra the avg spectra data
+#' @param ... other findpeaks parameters
 #'
 #' @export
 #' @return tbl_df
-pracma_findpeaks <- function(avg_spectra){
-  out_peaks <- pracma::findpeaks(avg_spectra$intensity)
+pracma_findpeaks <- function(avg_spectra, ...){
+  out_peaks <- pracma::findpeaks(avg_spectra$intensity, ...)
   mz_peaks <- avg_spectra[out_peaks[, 2], ]
   mz_peaks
+}
+
+#' find peaks
+#'
+#' @param avg_spectra input spectrum
+#' @param m how many on each side need to be lower
+#'
+#' @export
+#' @return tbl_df
+find_peaks <- function(avg_spectra, m = 3){
+  x <- avg_spectra$intensity
+  shape <- diff(sign(diff(x, na.pad = FALSE)))
+  pks <- lapply(which(shape < 0), FUN = function(i){
+    z <- i - m + 1
+    z <- ifelse(z > 0, z, 1)
+    w <- i + m + 1
+    w <- ifelse(w < length(x), w, length(x))
+    if(all(x[c(z : i, (i + 2) : w)] <= x[i + 1])) return(i + 1) else return(numeric(0))
+  })
+  pks <- unlist(pks)
+  avg_peaks <- avg_spectra[pks, ]
+  avg_peaks
 }
