1dd324935b225b21b76ab42d9cd3177fdd7f3688
Robert M Flight <r.flight79@gmail.com>
2016-07-18 10:49:04 -0400

    have functions that can convert peak_list to and from JSON

--- R/PeakPicking.R
+++ R/PeakPicking.R
@@ -15,3 +15,37 @@ PeakPickingAnalysis <- R6::R6Class("PeakPickingAnalysis",
     }
   )
 )
+
+#' PeakList to json
+#'
+#' takes a PeakList object, and generates a json version
+#'
+#' @param peak_list a data.frame or tbl_df to convert
+#'
+#' @export
+#' @return json_string
+#'
+peak_list_2_json <- function(peak_list){
+  assert_that(is.data.frame(peak_list))
+
+  peak_list2 <- list(Peaks = peak_list)
+  jsonlite::toJSON(peak_list2, auto_unbox = TRUE, pretty = TRUE)
+
+}
+
+#' PeakList from json
+#'
+#' takes json representing a PeakList object, and generates the data.frame version
+#'
+#' @param json_string the json to convert
+#' @param in_var the top level variable containing the "Peaks"
+#'
+#' @export
+#' @return tbl_df
+#'
+json_2_peak_list <- function(json_string, in_var = "Peaks"){
+
+  peak_list <- jsonlite::fromJSON(json_string)[[in_var]]
+  tibble::as_tibble(peak_list)
+
+}
