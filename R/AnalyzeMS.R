d308bed86a1bbbe8303fad71b4b3399e9eeac8dc
Robert M Flight <r.flight79@gmail.com>
2016-07-19 11:03:53 -0400

    revamp AnalyzeMS to do better initialization, and loading, and finally find the peaks

--- R/AnalyzeMS.R
+++ R/AnalyzeMS.R
@@ -6,42 +6,41 @@
 #' @export
 AnalyzeMS <- R6::R6Class("AnalyzeMS",
   public = list(
-   load_file = function(in_file, out_dir){
-     is_zip <- regexpr("*.zip", in_file)
-     is_mzML <- regexpr("*.mzML", in_file)
-
-     if (is_zip != -1) {
-       self$zip_ms <- ZipMS$new(in_file)
-     } else if (is_mzML != -1) {
-       self$zip_ms <- zip_ms_from_mzml(in_file, out_dir)
-       self$in_file <- self$zip_ms$zip_file
-     }
+   load_file = function(){
+     self$zip_ms <- ZipMS$new(self$in_file)
    },
+   found_peaks = NULL,
 
    find_peaks = function(){
-     self$zip_ms$peaks <- self$peak_finder(self$raw_ms)
+     self$found_peaks <- self$peak_finder(self$zip_ms$raw_ms$raw_data,
+                                          self$zip_ms$raw_ms$scan_range)
+     #self$zip_ms$add_peaks(found_peaks)
    },
 
    write_results = function(){
      self$zip_ms$write()
    },
 
-   set_peak_finder <- function(in_function){
+   set_peak_finder = function(in_function){
      self$peak_finder <- in_function
    },
 
-   initialize = function(in_file, out_dir = NULL, peak_finder = NULL){
+   peak_finder = NULL,
+
+   zip_ms = NULL,
+   in_file = NULL,
+   out_file = NULL,
+
+   initialize = function(in_file, out_file = NULL, peak_finder = NULL){
      self$in_file <- in_file
-     self$out_dir <- out_dir
+     if (!is.null(out_file)) {
+       self$out_file <- out_file
+     }
+
 
      if (!is.null(peak_finder)) {
        self$peak_finder <- peak_finder
      }
    }
-  ),
-  private = list(
-   zip_ms = NULL,
-   in_file = NULL,
-   peak_finder = NULL
   )
 )
