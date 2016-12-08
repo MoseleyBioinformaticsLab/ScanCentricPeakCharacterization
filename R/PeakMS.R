1f4320ba9ed7bf8fccafe8db26f904d80bcbcecc
Robert M Flight <r.flight79@gmail.com>
2016-12-08 15:35:11 -0500

    add a ScanMS class which stores ALL of the peaks associated with a scan of the instrument, and provides a method to get all of the peak_info

--- R/PeakMS.R
+++ R/PeakMS.R
@@ -29,6 +29,62 @@ PeakMS <- R6::R6Class("PeakMS",
 
       self$peak_type <- define_peak_type(peak_data, flat_cut)
       self$peak_data <- peak_data
+      invisible(self)
+    }
+  )
+)
+
+#' Storing all of the peaks associated with a scan
+#'
+#' Reference class to hold the results of peak finding of an entire "scan"
+#' within a mass-spectrum.
+#'
+#' @param scan_data a data.frame of mz and intensity
+#' @param min_points how many points to consider a "peak"
+#' @param n_peak how many peaks are allowed to be found
+#' @param flat_cut relative intensity to decide a peak is "flat"
+#'
+#' @return reference class with a list of peaks
+#'
+#' @export
+"ScanMS"
+
+ScanMS <- R6::R6Class("ScanMS",
+  public = list(
+    peaks = NULL,
+    get_peak_info = function(which_peak = NULL){
+      n_peak <- length(self$peaks)
+      if (is.null(which_peak)) {
+        which_peak <- seq(1, n_peak)
+      }
+      out_info <- lapply(self$peaks[which_peak], function(x){
+        tmp_info <- x$peak_info
+        tmp_info$peak <- x$peak_id
+        tmp_info
+      })
+      do.call(rbind, out_info)
+    },
+
+    initialize = function(scan_data, min_points = 5, n_peak = Inf, flat_cut = 0.98){
+      peak_locations <- pracma::findpeaks(scan_data$intensity, nups = floor(min_points/2),
+                                          ndowns = floor(min_points/2))
+      peak_locations <- matrix(peak_locations, ncol = 4, byrow = FALSE)
+      scan_data$log_int <- metabolomicsUtilities::log_with_min(scan_data$intensity)
+
+      if (is.infinite(n_peak)) {
+        n_peak <- nrow(peak_locations)
+      } else {
+        n_peak <- min(c(nrow(peak_locations), n_peak))
+      }
+
+      self$peaks <- lapply(seq(1, n_peak), function(in_peak){
+        #print(in_peak)
+        peak_loc <- seq(peak_locations[in_peak, 3], peak_locations[in_peak, 4])
+        out_peak <- PeakMS$new(mz_data[peak_loc, ], min_points = min_points, flat_cut = flat_cut)
+        out_peak$peak_id <- in_peak
+        out_peak
+      })
+      invisible(self)
     }
   )
 )
