6aab05704103b3d123b4d6b6d78d2bf2e79ae99b
Robert M Flight <r.flight79@gmail.com>
2017-03-18 16:25:11 -0400

    make it easier and more sane to figure out whether to use mc or lapply

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,11 @@
 l_or_mclapply <- function(...){
-  if ((Sys.info()["sysname"] %in% "Linux") && ("parallel" %in% installed.packages()[, "Package"]) && !is.null(getOption("mc.cores"))) {
+  if (is.null(getOption("mc.cores"))) {
+    n_core <- 1
+  } else if (getOption("mc.cores") >= 2) {
+    n_core <- getOption("mc.cores")
+  }
+
+  if ((Sys.info()["sysname"] %in% "Linux") && ("parallel" %in% installed.packages()[, "Package"]) && (n_core > 1)) {
     parallel::mclapply(...)
   } else {
     lapply(...)
