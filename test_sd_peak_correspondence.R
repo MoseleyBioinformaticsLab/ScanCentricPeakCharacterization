b0eed0542eca69514888bae3c9f5a3d8a7a9da1f
Robert M Flight <r.flight79@gmail.com>
2017-03-29 14:01:29 -0400

    bit more clean up

--- test_sd_peak_correspondence.R
+++
@@ -1,5 +0,0 @@
-library(SIRM.FTMS.peakPickingMethods)
-load("test_multi_scan.RData")
-fcp <- FindCorrespondenceScans$new(multi_scan, multiplier = 3, notify_progress = TRUE)
-
-save(fcp, file = "test_fcp.RData")
