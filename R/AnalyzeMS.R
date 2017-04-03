b6177169d67f494324840550d7e8815354a2dc7e
Robert M Flight <r.flight79@gmail.com>
2017-04-03 11:03:48 -0400

    first attempt at a function that does peak finding, correspondence, and creates the correct peak data structure
    closes #2

--- R/AnalyzeMS.R
+++ R/AnalyzeMS.R
@@ -49,3 +49,100 @@ AnalyzeMS <- R6::R6Class("AnalyzeMS",
    }
   )
 )
+
+#' peak finding and reporting
+#'
+#' Given a RawMS object, actually does peak finding at the scan level and then
+#' peak correspondence.
+#'
+#' @return list
+#' @export
+peak_finder <- function(raw_data, scan_range, method = "lm_weighted", noise_function = noise_sorted_peaklist){
+  # example data:
+  # load("zip_ms_example.RData")
+  # raw_data <- zip_ms$raw_ms
+  multi_scan <- SIRM.FTMS.peakCharacterization::MultiScans$new(raw_data, peak_method = method)
+  multi_scan_peak_list <- MultiScansPeakList$new(multi_scan, noise_function = noise_function)
+
+  correspondent_peaks <- FindCorrespondenceScans$new(multi_scan_peak_list, multiplier = 3)
+  correspondent_peaks$master_peak_list <- normalize_scans(correspondent_peaks$master_peak_list)
+
+  get_mz <- function(scan_mz, sd_model){
+    mean_mz <- mean(scan_mz)
+    median_mz <- median(scan_mz)
+    sd_mz <- sd(scan_mz)
+
+    model_sd = exponential_predict(sd_model, mean_mz)[1]
+    values = scan_mz
+
+    list(Mean = mean_mz,
+         Median = median_mz,
+         Sd = sd_mz,
+         ModelSD = model_sd,
+         Values = values)
+  }
+
+  get_height_area <- function(scan_height_area){
+    mean_h <- mean(scan_height_area)
+    median_h <- median(scan_height_area)
+    sd_h <- sd(scan_height_area)
+    rsd_h <- sd_h / mean_h
+    values <- scan_height_area
+
+    list(Mean = mean_h,
+         Median = median_h,
+         Sd = sd_h,
+         Rsd = rsd_h,
+         Values = values)
+  }
+
+
+
+  create_peak_data <- function(correspondent_peaks){
+    sd_model <- correspondent_peaks$sd_models[[1]] # grab the digital resolution model
+    master_peaks <- correspondent_peaks$master_peak_list
+    n_peak <- length(master_peaks$master)
+
+    peak_data <- lapply(seq(1, n_peak), function(in_peak){
+      tmp_index <- !is.na(master_peaks$scan_mz[in_peak, ])
+
+      mz <- get_mz(master_peaks$scan_mz[in_peak, tmp_index], sd_model)
+      height <- get_height_area(master_peaks$scan_height[in_peak, tmp_index])
+      area <- get_height_area(master_peaks$scan_area[in_peak, tmp_index])
+      norm_area <- lapply(area, function(x){x / mz$model_sd})
+
+      list(N = sum(tmp_index),
+           ObservedMZ = mz,
+           Height = height,
+           Area = area,
+           NormalizedArea = norm_area)
+    })
+    peak_data
+  }
+
+  peak_data <- create_peak_data(correspondent_peaks)
+
+  sample_meta <- list(Id = raw_data$raw_metadata$run$id,
+                      StartTime = raw_data$raw_metadata$run$startTimeStamp,
+                      Polarity = raw_data$raw_metadata$run$scanPolarity,
+                      Instrument = raw_data$raw_metadata$referenceableParamGroupList$referenceableParamGroup$cvParam.1$value,
+                      Sha1 = raw_data$raw_metadata$fileDescription$sourceFileList$sourceFile$cvParam.2$value)
+
+  function_call <- "peak_finder"
+  function_pkg <- find(function_call)
+  pkg_description <- utils::packageDescription(substring(function_pkg, 9))
+
+  if (!is.null(pkg_description$RemoteSha)) {
+    pkg_sha <- pkg_description$RemoteSha
+  } else {
+    pkg_sha <- ""
+  }
+
+  processing_meta <- list(CalledFunction = peak_finder,
+                          Method = method,
+                          NScan = length(multi_scan$scans))
+
+  list(Sample = sample_meta,
+       Processing = processing_meta,
+       Peaks = peak_data)
+}
