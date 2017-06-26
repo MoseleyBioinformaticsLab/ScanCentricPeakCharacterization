d3b19c10f437bd77baae61306ca9fa6c4f21a6ed
Robert M Flight <r.flight79@gmail.com>
2017-06-26 16:24:34 -0400

    bracket in wrong location

--- run_tests.R
+++ run_tests.R
@@ -1,6 +1,6 @@
 test_res <- as.data.frame(devtools::test())
 
-if (sum(test_res)$error > 0) {
+if (sum(test_res$error) > 0) {
   stop_code <- 1
 } else if (sum(test_res$failed) > 0) {
   stop_code <- 1
