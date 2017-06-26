f1e4a84e7a173621ca22fcfbb9b41526efafde2b
Robert M Flight <r.flight79@gmail.com>
2017-06-26 16:20:29 -0400

    adding other cases

--- run_tests.R
+++ run_tests.R
@@ -1,6 +1,10 @@
-test_res <- devtools::test()
+test_res <- as.data.frame(devtools::test())
 
-if (sum(test_res$error) > 0) {
+if (sum(test_res)$error > 0) {
+  stop_code <- 1
+} else if (sum(test_res$failed) > 0) {
+  stop_code <- 1
+} else if (sum(test_res$warning) > 0) {
   stop_code <- 1
 } else {
   stop_code <- 0
