4e9a34e1194a69b0a60428740101babafe1b5115
Robert M Flight <r.flight79@gmail.com>
2017-07-17 09:06:21 -0400

    ability to ignore warnings in unit testing

--- run_tests.R
+++ run_tests.R
@@ -1,10 +1,15 @@
 test_res <- as.data.frame(devtools::test())
 
+warnings_as_errors <- Sys.getenv("warnings_as_errors")
+if (is.null(warnings_as_errors)) {
+  warnings_as_errors <- TRUE
+}
+
 if (sum(test_res$error) > 0) {
   stop_code <- 1
 } else if (sum(test_res$failed) > 0) {
   stop_code <- 1
-} else if (sum(test_res$warning) > 0) {
+} else if ((sum(test_res$warning) > 0) && (warnings_as_errors)) {
   stop_code <- 1
 } else {
   stop_code <- 0
