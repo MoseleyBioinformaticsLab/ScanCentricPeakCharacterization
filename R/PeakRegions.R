dae6a98a0f727dab9ae1407b5b0f126f0f6c8fc6
Robert M Flight <r.flight79@gmail.com>
2018-09-29 17:06:15 -0400

    setting all of our initial data up in terms of frequency instead of m/z to make region finding and definition easier

--- R/PeakRegions.R
+++ R/PeakRegions.R
@@ -1,91 +1,70 @@
-#' mz points to mz regions
+#' mz points to frequency regions
 #'
 #' Given M/Z point data in a data.frame, create IRanges based point "regions" of
 #' width 1, using the `point_multiplier` argument to convert from the floating
 #' point double to an `integer`.
 #'
-#' @param mz_data a `data.frame` with at least a column named `mz`
-#' @param point_multiplier a value used to convert to integers.
+#' @param data a `data.frame` with either `m/z` or `frequency`
+#' @param frequency_multiplier a value used to convert to integers.
 #'
 #' @importFrom IRanges IRanges
 #' @importFrom S4Vectors mcols
 #' @export
-mz_points_to_regions <- function(mz_data, point_multiplier = 20000){
-  mz_regions <- IRanges(start = round(mz_data[, "mz"] * point_multiplier), width = 1)
-  if (is.null(mz_data$point)) {
-    mz_data$point <- seq(1, nrow(mz_data))
+mz_points_to_frequency_regions <- function(mz_data, frequency_model = NULL, point_multiplier = 1000){
+  if (("frequency" %in% names(mz_data)) & (is.null(frequency_model))) {
+    frequency_data = mz_data
   }
-  S4Vectors::mcols(mz_regions) <- mz_data
-  mz_regions@metadata <- list(point_multiplier = point_multiplier)
-  mz_regions
+
+  if (!is.null(frequency_model)) {
+    frequency_data = mz_scans_to_frequency(mz_data, frequency_model)
+  }
+
+  frequency_regions <- IRanges(start = round(frequency_data[, "frequency"] * point_multiplier), width = 1)
+  if (is.null(frequency_data$point)) {
+    frequency_data$point <- seq(1, nrow(frequency_data))
+  }
+  S4Vectors::mcols(frequency_regions) <- frequency_data
+  frequency_regions@metadata <- list(point_multiplier = point_multiplier)
+  frequency_regions
 }
 
-#' create mz regions
+#' create frequency regions
 #'
-#' Given an M/Z data.frame model and a range, create IRanges based regions
+#' Given a point-point spacing and a frequency range, create IRanges based regions
 #' of specified width. Overlapping sliding regions can be creating by specifying
 #' a `region_size` bigger than `delta`, adjacent tiled regions can be created
 #' by specifying a `region_size` == `delta`.
 #'
-#' @param mz_model data.frame where `x` is M/Z, `y` is difference in M/Z.
-#' @param use_range the range of M/Z to use
+#' @param point_spacing how far away are subsequent points.
+#' @param frequency_range the range of frequency to use
 #' @param region_size how *big* is each of the regions
 #' @param delta the *step* size between the beginning of each subsequent region
 #' @param point_multiplier multiplier to convert from M/Z to integer space
 #'
-#' @details For Fourier-transform mass spec, points are often equally spaced
+#' @details For Fourier-transform mass spec, points are equally spaced in
 #'   frequency space, which will lead to unequal spacing in M/Z space. Therefore,
-#'   to create regions that properly cover the same number of M/Z points, the spacing
-#'   between M/Z points has to increase as M/Z space. This leads to needing the
-#'   `mz_model`, where `x` is an M/Z value, and `y` is the spacing between subsequent
-#'   M/Z points.
+#'   we create regions using the point-point differences in frequency space.
 #'
 #'   What will be returned is an `IRanges` object, where the widths are constantly
 #'   increasing over M/Z space.
 #'
 #' @export
 #' @return IRanges
-create_mz_regions <- function(mz_model, use_range = NULL,
+create_frequency_regions <- function(point_spacing = 0.5, frequency_range = NULL,
                               region_size = 10, delta = 1,
-                              point_multiplier = 20000){
-  if (is.null(use_range)) {
-    range_start = floor(min(mz_model$x))
-    range_end = ceiling(max(mz_model$x))
-  } else {
-    range_start = floor(min(use_range))
-    range_end <- ceiling(max(use_range))
+                              point_multiplier = 1000){
+  if (is.null(frequency_range)) {
+    stop("A valid range of frequencies must be supplied!")
   }
 
-  min_spacing <- min(mz_model$y) * delta
-  mz_start <- vector(mode = "numeric", length = (range_end - range_start) / min_spacing)
-  mz_end <- mz_start
+  use_frequencies = round(frequency_range)
 
-  start_region <- range_start
-  iteration <- 1
+  frequency_start = round(seq(use_frequencies[1], use_frequencies[2] - region_size, by = point_spacing * delta) * point_multiplier)
+  frequency_end = round(seq(use_frequencies[1] + region_size, use_frequencies[2], by = point_spacing * delta) * point_multiplier)
 
-  while (start_region < range_end) {
-    mz_start[iteration] <- start_region
-    curr_spacing <- mz_model[which.min(abs(mz_model$x - start_region)), "y"]
-    mz_end[iteration] <- start_region + (region_size * curr_spacing)
-    start_region <- start_region + (curr_spacing * delta)
-    iteration <- iteration + 1
-  }
+  regions <- IRanges::IRanges(start = frequency_start, end = frequency_end)
 
-  keep_locs <- mz_start != 0
-  mz_start <- mz_start[keep_locs]
-  mz_end <- mz_end[keep_locs]
-
-  if (is.null(point_multiplier)) {
-    point_multiplier <- 1 / min(mz_model$y)
-  }
-
-
-  region_start <- round(mz_start * point_multiplier)
-  region_end <- round(mz_end * point_multiplier)
-
-  regions <- IRanges::IRanges(start = region_start, end = region_end)
-
-  S4Vectors::mcols(regions) <- list(mz_start = mz_start, mz_end = mz_end)
+  #S4Vectors::mcols(regions) <- list(mz_start = mz_start, mz_end = mz_end)
   regions@metadata <- list(point_multiplier = point_multiplier,
                            delta = delta,
                            region_size = region_size)
@@ -96,8 +75,8 @@ create_mz_regions <- function(mz_model, use_range = NULL,
 #' @export
 PeakRegions <- R6::R6Class("PeakRegions",
   public = list(
-    mz_point_regions = NULL,
-    mz_model = NULL,
+    frequency_point_regions = NULL,
+    frequency_model = NULL,
 
     peak_regions = NULL,
     sliding_regions = NULL,
@@ -132,31 +111,47 @@ PeakRegions <- R6::R6Class("PeakRegions",
       invisible(self)
     },
 
-    add_data = function(mz_data, mz_model = NULL){
-      self$mz_point_regions <- mz_points_to_regions(mz_data, self$point_multiplier)
-      tmp_model <- loess_to_df(mz_model)
-
-      if (!is.null(mz_model)) {
-        self$mz_model <- tmp_model[tmp_model$which %in% "fitted", ]
+    add_data = function(raw_ms = NULL, frequency_data = NULL){
+      if (!is.null(raw_ms) & !is.null(frequency_data)){
+        stop("Only pass in either raw_ms or frequency_data, not both!")
       }
 
-      self$mz_range <- range(mz_data$mz)
+      if (is.null(raw_ms) & is.null(frequency_data)) {
+        stop("One of raw_ms or frequency_data must be supplied!")
+      }
+
+      if (!is.null(raw_ms)) {
+        raw_mz_data = raw_ms$extract_raw_data()
+        self$frequency_model = frequency_models_scans(raw_mz_data)
+        self$frequency_point_regions = mz_points_to_frequency_regions(raw_mz_data,
+                                                                      self$frequency_model,
+                                                                      self$frequency_multiplier)
+      }
+
+      if (!is.null(frequency_data)) {
+        self$frequency_model = create_frequency_model(frequency_data)
+
+        self$frequency_point_regions = mz_points_to_frequency_regions(frequency_data,
+                                                                      frequency_model = NULL,
+                                                                      point_multiplier = self$frequency_multiplier)
+      }
+      self$frequency_point_regions <- mz_points_to_frequency_regions(raw_ms$extract_raw_data(), self$frequency_multiplier)
 
       self$set_min_scan()
       invisible(self)
     },
 
-    initialize = function(mz_data = NULL, mz_model = NULL,
-                          point_multiplier = 200000, frequency_multiplier = 1000,
+    initialize = function(raw_ms = NULL,
+                          frequency_data = NULL,
+                          point_multiplier = 200000,
+                          frequency_multiplier = 1000,
                           scan_perc = 0.1, max_subsets = 100){
       self$point_multiplier <- point_multiplier
       self$frequency_multiplier <- frequency_multiplier
       self$scan_perc <- scan_perc
       self$max_subsets <- max_subsets
 
-      if (!is.null(mz_data)) {
-        self$add_data(mz_data, mz_model)
-      }
+      self$add_data(raw_ms, frequency_data)
 
       invisible(self)
     }
