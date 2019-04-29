924f724188b8e239225c37f476224d53ad088a97
rmflight <r.flight79@gmail.com>
2019-04-29 15:48:31 -0400

    actually doing the transformation from M/Z to frequency using a single linear model, and verifying that the ordering in frequency space is preserved compared to M/Z space.
    It is, and we have a value of -1 spearman correlation

--- debugging_frequency_regions.R
+++ debugging_frequency_regions.R
@@ -9,7 +9,113 @@ prf = PeakRegionFinder$new(pr)
 prf$add_regions()
 self = prf
 self$reduce_sliding_regions()
-use_regions = seq(1, 100)
+use_regions = self$peak_regions$peak_regions[1:1000]
+
+get_reduced_peaks_safely <- function(in_range, peak_method = "lm_weighted", min_points = 4,
+                              which = c("mz", "frequency")){
+  range_point_data <- in_range@elementMetadata
+
+  possible_peaks <- pracma::findpeaks(range_point_data$log_int, nups = round(min_points / 2))
+
+  if (!is.null(possible_peaks)) {
+    n_peak <- nrow(possible_peaks)
+    peaks <- purrr::map_df(seq(1, n_peak), function(in_peak){
+      #print(in_peak)
+      "!DEBUG Peak `in_peak`"
+      peak_loc <- seq(possible_peaks[in_peak, 3], possible_peaks[in_peak, 4])
+      peak_data <- range_point_data[peak_loc, ]
+      weights <- peak_data$intensity / max(peak_data$intensity)
+      out_peak = purrr::map(which, purrr::safely(function(in_which){
+        tmp_peak = get_fitted_peak_info(peak_data, use_loc = in_which, w = weights)
+        names(tmp_peak) = paste0(names(tmp_peak), ".", in_which)
+        tmp_peak
+      }))
+      if (any(purrr::map_lgl(out_peak, ~ is.null(.x$result)))) {
+        out_peak <- purrr::map_dfc(which, function(in_which){
+          tmp_peak = create_na_peak()
+          names(tmp_peak) = paste0(names(tmp_peak), ".", in_which)
+          tmp_peak
+        })
+      } else {
+        out_peak = purrr::map_dfc(out_peak, ~ .x$result)
+      }
+      #out_peak = do.call(rbind, out_peak[!null_peak])
+      #out_peak <- get_fitted_peak_info(peak_data, w = weights)
+      #out_peak <- get_peak_info(range_data[peak_loc, ], peak_method = peak_method, min_points = min_points)
+      #out_peak$points <- I(list(IRanges::start(in_range)[peak_loc]))
+      out_peak$scan <- range_point_data[peak_loc[1], "scan"]
+      out_peak$ipeak = in_peak
+      out_peak
+    })
+  } else {
+    peaks <- purrr::map_dfc(which, function(in_which){
+      tmp_peak = create_na_peak()
+      names(tmp_peak) = paste0(names(tmp_peak), ".", in_which)
+      tmp_peak
+    })
+    peaks$points <- NA
+    peaks$scan <- range_point_data$scan[1]
+  }
+  peaks
+}
+
+original_points = self$peak_regions$frequency_point_regions
+
+fit_points = as.data.frame(original_points@elementMetadata) %>% dplyr::filter(convertable, scan == 159) %>%
+  dplyr::select(mean_mz, mean_frequency) %>%
+  dplyr::mutate(mz = mean_mz, frequency = mean_frequency)
+fit_model = lm(frequency ~ I(1 / (mz^0.5)), data = fit_points)
+fit_model$coefficients = c(4, 29803531)
+
+new_points = original_points
+new_data = as.data.frame(new_points@elementMetadata)
+new_data$frequency = predict(fit_model, newdata = new_data)
+new_data$log_int = log(new_data$intensity + 1e-18)
+
+S4Vectors::mcols(new_points) = new_data
+
+original_data = S4Vectors::mcols(original_points)
+original_data$frequency = original_data$mean_frequency
+original_data$log_int = log(original_data$intensity + 1e-18)
+S4Vectors::mcols(original_points) = original_data
+original_points_trim = IRanges::subsetByOverlaps(original_points, use_regions)
+original_points_scan = split(original_points_trim, S4Vectors::mcols(original_points_trim)$scan)
+
+original_reduced = purrr::map(original_points_scan, get_reduced_peaks_safely)
+original_reduced = purrr::map_df(original_reduced, ~ .x)
+
+new_points_trim = IRanges::subsetByOverlaps(new_points, use_regions)
+new_points_scan = split(new_points_trim, S4Vectors::mcols(new_points_trim)$scan)
+new_reduced = purrr::map(new_points_scan, get_reduced_peaks_safely)
+new_reduced = purrr::map_df(new_reduced, ~ .x)
+
+
+ggplot(original_reduced, aes(x = ObservedCenter.mz, y = ObservedCenter.frequency)) + geom_point()
+cor(original_reduced[, c("ObservedCenter.mz", "ObservedCenter.frequency")], method = "spearman", use = "complete.obs")
+
+ggplot(new_reduced, aes(x = ObservedCenter.mz, y = ObservedCenter.frequency)) + geom_point()
+cor(new_reduced[, c("ObservedCenter.mz", "ObservedCenter.frequency")], method = "spearman", use = "complete.obs")
+new_reduced = dplyr::mutate(new_reduced, mz = ObservedCenter.mz, frequency = ObservedCenter.frequency)
+new_reduced_points = mz_points_to_frequency_regions(new_reduced)
+
+new_reduced_ranges = create_frequency_regions(point_spacing = 0.5, n_point = 1000, delta_point = 1000,
+                                              frequency_range = range(new_reduced$ObservedCenter.frequency, na.rm = TRUE),
+                                              point_multiplier = 500)
+
+in_range = purrr::map_df(seq(1, length(new_reduced_ranges)), function(use_range){
+  tmp_points = as.data.frame(S4Vectors::mcols(IRanges::subsetByOverlaps(new_reduced_points, new_reduced_ranges[use_range])))
+  tmp_points = dplyr::filter(tmp_points, !is.na(mz), !is.na(frequency))
+  if (nrow(tmp_points) > 0) {
+    data.frame(irange = use_range,
+               cor = cor(tmp_points$mz, tmp_points$frequency, method = "spearman"))
+  } else {
+    NULL
+  }
+})
+
+# to be honest, this isn't the fairest comparison, because the mean_frequency isn't exactly right due to "issues",
+# but if everything lines up perfectly in frequency space compared to M/Z space, then that says that
+# this appears to just "work"
 peak_data <- split_regions(self$peak_regions$peak_regions[use_regions], self$peak_regions$frequency_point_regions, self$peak_regions$tiled_regions, peak_method = self$peak_method, min_points = self$min_points)
 
 p1_region = IRanges::subsetByOverlaps(self$peak_regions$frequency_point_regions, peak_data$regions[1])
