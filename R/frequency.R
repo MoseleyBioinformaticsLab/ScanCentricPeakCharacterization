f2301ce9b149edbcf2fe6a9fa0ded70ff90f036b
rmflight <r.flight79@gmail.com>
2019-05-02 16:19:42 -0400

    filtering out scans based on having outlier intercepts from their fitted frequency ~ mz models, because they appear to be some of the same bad scans we've seen before using LOESS based distances

--- R/frequency.R
+++ R/frequency.R
@@ -135,19 +135,26 @@ mz_scans_to_frequency = function(mz_scan_df, ...){
 
   frequency_coefficients = internal_map$map_function(mz_frequency, function(in_freq){
     use_peaks = in_freq$convertable
-    fit_frequency_sr(in_freq$mean_mz[use_peaks], in_freq$mean_frequency[use_peaks])$coefficients
+    tmp_fit = fit_frequency_sr(in_freq$mean_mz[use_peaks], in_freq$mean_frequency[use_peaks])$coefficients
+    data.frame(intercept = tmp_fit[1], slope = tmp_fit[2], scan = in_freq[1, "scan"])
   })
 
   frequency_coefficients = do.call(rbind, frequency_coefficients)
 
-  model_coefficients = c(median(frequency_coefficients[, 1]), median(frequency_coefficients[, 2]))
+  bad_coefficients = boxplot.stats(frequency_coefficients$intercept)$out
+
+  frequency_coefficients = dplyr::filter(frequency_coefficients, !(intercept %in% bad_coefficients))
+
+  model_coefficients = c(median(frequency_coefficients$intercept), median(frequency_coefficients$slope))
 
   mz_frequency_df = do.call(rbind, mz_frequency)
+
+  mz_frequency_df = mz_frequency_df[mz_frequency_df$scan %in% frequency_coefficients$scan, ]
 
   mz_frequency_df$frequency = predict_frequency_sr(mz_frequency_df$mz, model_coefficients)
 
   rownames(mz_frequency_df) = NULL
-  list(frequency = mz_frequency_df, coefficients = model_coefficients)
+  list(frequency = mz_frequency_df, all_coefficients = frequency_coefficients, coefficients = model_coefficients)
 }
 
 #' convert mz to frequency using linear fit
