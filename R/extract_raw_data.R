c4a0224bd942f7b2e34865063bfd64be3fae3ea5
rmflight <r.flight79@gmail.com>
2019-06-11 14:20:10 -0400

    adding comments that explain what each bit does

--- R/extract_raw_data.R
+++ R/extract_raw_data.R
@@ -10,20 +10,36 @@
 #' @return string or data.frame
 extract_raw_method = function(in_file, output_type = "data.frame"){
   in_file2 = normalizePath(in_file)
-  sys_data = system2("hexdump", args = c("-C", in_file2), stdout = TRUE)
-  pipe_pattern = "(?<=\\|)[^|]++(?=\\|)"
 
+  # performs a hexdump of the binary file, with translation,
+  # and captures it in a text string
+  # Output (on linux) looks like:
+  #
+  # 02 .... |translated contents|
+  # .
+  # .
+  sys_data = system2("hexdump", args = c("-C", in_file2), stdout = TRUE)
+
+  # this pattern extracts the translated contents between the pipe on each line
+  pipe_pattern = "(?<=\\|)[^|]++(?=\\|)"
   pipe_content = stringr::str_extract(sys_data, pipe_pattern)
 
+  # the output has weird dots embedded. Two or more dots seem to indicate spaces,
+  # whereas single dots should just be removed. Hence the substitution below
   pipe_nodots = gsub("\\.", "", gsub("\\.\\.", " ", pipe_content))
 
+  # In my experience (with a small number of files), the method
+  # is normally embedded in the first 2000 translated lines
   pipe_pasted = paste(pipe_nodots[1:2000], sep = "", collapse = "")
 
-  method_pattern = "Method Summary.* New Method"
-
+  # extract the method definition
+  method_pattern = "Method Summary.*New Method"
   method_text = stringr::str_extract(pipe_pasted, method_pattern)
+
+  # and split across multiple spaces
   method_split = strsplit(method_text, "  ")[[1]]
 
+  # now we push it into a data.frame
   has_char = nchar(method_split) > 0
   method_split = method_split[has_char]
   method_split = method_split[2:(length(method_split) - 1)]
