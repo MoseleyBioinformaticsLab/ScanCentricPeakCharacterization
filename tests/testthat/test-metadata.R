05e94a522308816df48f9559590f48dc01e55fd2
rmflight <r.flight79@gmail.com>
2022-05-26 13:11:04 -0400

    trying to figure out why this fails during check

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -1,25 +1,30 @@
-create_in_temp <- function(dir_loc, create_it = TRUE) {
-  temp_path <- tempfile(pattern = paste0("metadata-test-", dir_loc))
-  if (create_it) {
-    dir.create(temp_path)
+create_in_temp <- function(dir_loc) {
+  temp_path <- tempdir(check = TRUE)
+  full_path = file.path(temp_path, dir_loc)
+  is_create = dir.create(full_path, recursive = TRUE)
+  if (!is_create) {
+    stop("Test temp directory not created!")
   }
-  temp_path
+  full_path
 }
+
+with_loc <- create_in_temp("with_metadata")
+without_loc <- create_in_temp("without_metadata")
 
 erase <- function(path) unlink(path, recursive = TRUE)
 
 test_that("with and without metadata works", {
-  with_loc <- create_in_temp("with_metadata")
+
   mzml_file = system.file("extdata", "lipid_example.mzML", package = "ScanCentricPeakCharacterization")
   json_file = system.file("extdata", "lipid_example.json", package = "ScanCentricPeakCharacterization")
-  test_analyzer <- SCZip$new(mzml_file, json_file, temp_loc = with_loc)
+  test_analyzer <- SCZip$new(mzml_file, mzml_meta_file = json_file, temp_loc = with_loc)
 
   expect_true(!is.null(test_analyzer$sc_mzml$mzml_metadata$file))
 
-  without_loc <- create_in_temp("without_metadata")
-  test_analyzer2 <- SCZip$new(mzml_file, temp_loc = without_loc)
+  test_analyzer2 <- SCZip$new(mzml_file)
   expect_false("file" %in% names(test_analyzer2$sc_mzml$mzml_metadata))
 
-  erase(with_loc)
-  erase(without_loc)
 })
+
+erase(with_loc)
+erase(without_loc)
