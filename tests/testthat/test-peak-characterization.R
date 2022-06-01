ae2f088d649fcb841a1f078c415857ea59a0b83a
rmflight <r.flight79@gmail.com>
2022-06-01 17:18:58 -0400

    moved everything into a separate testing directory so that it isnt so big

--- tests/testthat/test-peak-characterization.R
+++ tests/testthat/test-peak-characterization.R
@@ -1,43 +1,44 @@
-run_peakcharacterization <- as.logical(Sys.getenv("RUN_PEAKCHARACTERIZATION"))
-if (is.na(run_peakcharacterization)) {
-  run_peakcharacterization <- FALSE
+test_peakcharacterization <- as.logical(Sys.getenv("test_peakcharacterization"))
+if (is.na(test_peakcharacterization)) {
+  test_peakcharacterization <- FALSE
 }
 
 test_that("peak characterization steps run", {
 
-  skip_if_not(run_peakcharacterization)
+  top_level = "/home/rmflight/Projects/work/ScanCentricPeakCharacterizationCharacterizationRef"
+  skip_if_not(test_peakcharacterization)
 
   library(furrr)
   plan(multicore(workers = 5))
   set_internal_map(furrr::future_map)
 
-  starting_mzml = readRDS("init_mzml.rds")
+  starting_mzml = readRDS(file.path(top_level, "init_mzml.rds"))
 
   p2 = SCPeakRegionFinder$new(starting_mzml)
 
   p2$add_regions()
   p2$reduce_sliding_regions()
-  expect_equal(p2$peak_regions$peak_regions, readRDS("reduce_sliding_regions-peak_regions.rds"))
+  expect_equal(p2$peak_regions$peak_regions, readRDS(file.path(top_level, "reduce_sliding_regions-peak_regions.rds")))
 
   p2$split_peak_regions()
-  expect_equal(p2$peak_regions$peak_region_list, readRDS("split_peak_regions-peak_region_list.rds"))
+  expect_equal(p2$peak_regions$peak_region_list, readRDS(file.path(top_level, "split_peak_regions-peak_region_list.rds")))
 
   p2$remove_double_peaks_in_scans()
-  expect_equal(p2$peak_regions$peak_region_list, readRDS("remove_double_peaks_in_scans-peak_region_list.rds"))
+  expect_equal(p2$peak_regions$peak_region_list, readRDS(file.path(top_level, "remove_double_peaks_in_scans-peak_region_list.rds")))
 
   p2$normalize_data()
-  expect_equal(p2$peak_regions$normalization_factors, readRDS("normalize_data-normalization_factors.rds"))
-  expect_equal(p2$peak_regions$frequency_point_regions, readRDS("normalize_data-frequency_point_regions.rds"))
+  expect_equal(p2$peak_regions$normalization_factors, readRDS(file.path(top_level, "normalize_data-normalization_factors.rds")))
+  expect_equal(p2$peak_regions$frequency_point_regions, readRDS(file.path(top_level, "normalize_data-frequency_point_regions.rds")))
 
   p2$find_peaks_in_regions()
-  expect_equal(p2$peak_regions$peak_data, readRDS("find_peaks_in_regions-peak_data.rds"))
-  expect_equal(p2$peak_regions$scan_level_arrays, readRDS("find_peaks_in_regions-scan_level_arrays.rds"))
+  expect_equal(p2$peak_regions$peak_data, readRDS(file.path(top_level, "find_peaks_in_regions-peak_data.rds")))
+  expect_equal(p2$peak_regions$scan_level_arrays, readRDS(file.path(top_level, "find_peaks_in_regions-scan_level_arrays.rds")))
 
   p2$indicate_high_frequency_sd()
-  expect_equal(p2$peak_regions$peak_data, readRDS("indicate_high_frequency_sd-peak_data.rds"))
+  expect_equal(p2$peak_regions$peak_data, readRDS(file.path(top_level, "indicate_high_frequency_sd-peak_data.rds")))
 
   p2$add_offset()
-  expect_equal(p2$peak_regions$peak_data, readRDS("add_offset-peak_data.rds"))
+  expect_equal(p2$peak_regions$peak_data, readRDS(file.path(top_level, "add_offset-peak_data.rds")))
 
   rm(p2)
   rm(starting_mzml)
